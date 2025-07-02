18ae8d52d61c69e7a9bd26930fa734d7d4a03cbf
lizhiyu022 <lizhiyu022@ke.com>
2025-07-02 14:26:57 +0800

    feat: add theme mode

--- sidebar/scripts.tsx
+++ sidebar/scripts.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import './styles.css';
+import { ThemeProvider } from './components/ThemeProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
